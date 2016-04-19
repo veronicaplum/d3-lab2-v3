378ea83d4d68555372442c05259e2f5f079a6e7a
veronicaplum <veronicaplum@tests-MacBook-Pro-9.local>
2016-04-19 15:02:03 -0500

    Module 8 assignment completed
    no graticule since it looks funny on my selected albersUSA()
    projection. Data reads, shows and projects.

--- js/main.js
+++ js/main.js
@@ -5,47 +5,38 @@ function setMap(){
      var width = 960,
         height = 460;
 
-    //create new svg container for the map
+    //svg map container created
     var map = d3.select("body")
         .append("svg")
         .attr("class", "map")
         .attr("width", width)
         .attr("height", height);
-/*
-    var projection = d3.geo.albers()
-        .center([0, 46.33])
-        .rotate([106.45, -0.91, 0])
-        .parallels([40.18, 54.69])
-        .scale(503.03)
-        .translate([(width / 2, height) / 2]);
-    */
-    
-    
+
+    //USA Albers projection
     var projection = d3.geo.albersUsa()
         .scale(1000)
         .translate([width / 2, height / 2]);
     
+    //path is the projection
     var path = d3.geo.path()
         .projection(projection);
     
     //use queue.js to parallelize asynchronous data loading
+    //note- I will be using UnitedStates.topojson , and the April5 version is a backup, but otherwise not used. 
     d3_queue.queue()
         .defer(d3.csv, "data/UnitedStates.csv") //load attributes from csv
-        .defer(d3.json, "data/UnitedStates.topojson") //load background spatial data
-        .defer(d3.json, "data/UnitedStatesApril5.topojson") //load choropleth spatial data
+        .defer(d3.json, "data/UnitedStates.topojson") //initial spatial data w/ attributes in topojson
+        .defer(d3.json, "data/UnitedStatesApril5.topojson") //backup spatialdata
         .await(callback);
 
     function callback(error, csvData, US, US45){
 
-        console.log(US);
- 
-        
         //translate europe TopoJSON
         var USfeatures= topojson.feature(US, US.objects.UnitedStates).features;
-       
         console.log(USfeatures);
     
-        var states = map.selectAll(".states")
+        //creates class names for each state indiividually(located at USfeatures.properties.State) and draws them to the map svg
+        var states = map.selectAll(".states") 
             .data(USfeatures) 
             .enter()
             .append("path")
@@ -53,27 +44,6 @@ function setMap(){
                 console.log(d);
                 return "states " + d.properties.State;
             })
-            .attr("d", path);
-        
-        
-        
-        /*
-         var countries = map.append("path")
-            .datum(europeCountries)
-            .attr("class", "countries")
-            .attr("d", path);
-
-        //add France regions to map
-        var regions = map.selectAll(".regions")
-            .data(franceRegions)
-            .enter()
-            .append("path")
-            .attr("class", function(d){
-                return "regions " + d.properties.adm1_code;
-            })
-            .attr("d", path);
-        */
-        
-        
+            .attr("d", path); //"d" is the class name created for each state from the statement d.properties.State and added in to the "states" class. Now each there are 51 states attributes, each continang there own class titled by their state initials.
     };
 };
