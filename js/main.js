1d3132c2af24de9556dc6e07d55381432fe63ec9
veronicaplum <veronicaplum@tests-MacBook-Pro-9.local>
2016-04-19 14:41:00 -0500

    With Graticule
    Looks funny since AK, Hawaii and US have different graidcules. Looks
    strange bunched together

--- js/main.js
+++ js/main.js
@@ -44,6 +44,17 @@ function setMap(){
         var USfeatures= topojson.feature(US, US.objects.UnitedStates).features;
        
         console.log(USfeatures);
+        var graticule = d3.geo.graticule()
+            .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
+
+        //create graticule lines
+        var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+            .data(graticule.lines()) //bind graticule lines to each element to be created
+            .enter() //create an element for each datum
+            .append("path") //append each element to the svg as a path element
+            .attr("class", "gratLines") //assign class for styling
+            .attr("d", path); //project graticule lines
+
     
         var states = map.selectAll(".states")
             .data(USfeatures) 
@@ -55,6 +66,24 @@ function setMap(){
             })
             .attr("d", path);
         
+        
+        
+        /*
+         var countries = map.append("path")
+            .datum(europeCountries)
+            .attr("class", "countries")
+            .attr("d", path);
+
+        //add France regions to map
+        var regions = map.selectAll(".regions")
+            .data(franceRegions)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                return "regions " + d.properties.adm1_code;
+            })
+            .attr("d", path);
+        */
         
         
     };
