e5799ce8f7eff13e8de87e4efd85ef3d268cbd8e
veronicaplum <veronicaplum@tests-MacBook-Pro-9.local>
2016-04-19 14:19:00 -0500

    Map Shows Up

--- js/main.js
+++ js/main.js
@@ -57,23 +57,5 @@ function setMap(){
         
         
         
-        /*
-         var countries = map.append("path")
-            .datum(europeCountries)
-            .attr("class", "countries")
-            .attr("d", path);
-
-        //add France regions to map
-        var regions = map.selectAll(".regions")
-            .data(franceRegions)
-            .enter()
-            .append("path")
-            .attr("class", function(d){
-                return "regions " + d.properties.adm1_code;
-            })
-            .attr("d", path);
-        */
-        
-        
     };
 };
