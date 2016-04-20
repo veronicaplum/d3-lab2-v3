4689954a44eeff275bb94eb33ef53f229a80cab3
veronicaplum <veronicaplum@tests-MacBook-Pro-9.local>
2016-04-19 19:35:52 -0500

    Changed cvs again
    now using UnitedStatesFin.csv

--- js/main.js
+++ js/main.js
@@ -24,17 +24,46 @@ function setMap(){
     //use queue.js to parallelize asynchronous data loading
     //note- I will be using UnitedStates.topojson , and the April5 version is a backup, but otherwise not used. 
     d3_queue.queue()
-        .defer(d3.csv, "data/UnitedStates.csv") //load attributes from csv
+        .defer(d3.csv, "data/UnitedStatesFin.csv") //load attributes from csv
         .defer(d3.json, "data/UnitedStatesFinal.topojson") //initial spatial data w/ attributes in topojson
         .defer(d3.json, "data/UnitedStatesApril5.topojson") //backup spatialdata
         .await(callback);
 
     function callback(error, csvData, US, US45){
+        console.log(csvData);
         console.log(US45);
+        
         //translate europe TopoJSON
         var USfeatures= topojson.feature(US, US.objects.UnitedStates).features;
         console.log(USfeatures);
     
+        var attrArray = ["var1", "var2", "var3", "var4", "var5", "var6","var7", "var8"];
+        
+        for (var i=0; i<csvData.length; i++){
+            var csvState = csvData[i]; //the current state
+            var csvKey = csvState.State; //the CSV primary key
+            
+            //loop through geojson regions to find correct region
+            for (var a=0; a<USfeatures.length; a++){
+                var geojsonProps = USfeatures[a].properties; //the current region geojson properties
+                var geojsonKey = geojsonProps.State; //the geojson primary key
+                
+                  //where primary keys match, transfer csv data to geojson properties object
+                if (geojsonKey == csvKey){
+                     //assign all attributes and values
+                    attrArray.forEach(function(attr){
+                        console.log(typeof(attr));
+                        var val = parseFloat(csvState[attr]); //get csv attribute value
+                        geojsonProps[attr] = val; //assign attribute and value to geojson properties
+                    });
+            };
+        };
+    };
+
+            
+            //for each variable in the current state
+                
+        
         //creates class names for each state indiividually(located at USfeatures.properties.State) and draws them to the map svg
         var states = map.selectAll(".states") 
             .data(USfeatures) 
